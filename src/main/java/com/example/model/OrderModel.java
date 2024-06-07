5dac9c06c2e5d75fd59568bd487de7aac55638ff
chenti <kate910131@gmail.com>
2024-06-08 00:55:04 +0800

    Add order & product model

--- src/main/java/com/example/model/OrderModel.java
+++ src/main/java/com/example/model/OrderModel.java
@@ -1,2 +1,43 @@
-package com.example.model;public class OrderModel {
+package com.example.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderModel {
+    private String orderID;
+    private String memberID;
+    private int price;
+    private boolean payStatus;
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public String getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(String memberID) {
+        this.memberID = memberID;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean ispayStatus() {
+        return payStatus;
+    }
+
+    public void setPayStatus(boolean payStatus) {
+        this.payStatus = payStatus;
+    }
 }
--- src/main/java/com/example/model/ProductModel.java
+++ src/main/java/com/example/model/ProductModel.java
@@ -1,2 +1,45 @@
-package com.example.model;public class ProductModel {
+package com.example.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductModel {
+    private String productID;
+    private String productName;
+    private int price;
+    private int quantity;
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
+
+
